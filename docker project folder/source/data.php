8eecd2d00f4eaaa6fa9d04a11dec9fd9f7f6e090
Ben <24175@ma-web.nl>
2023-10-11 12:05:03 +0200

    update

---
+++ docker project folder/source/data.php
@@ -0,0 +1,21 @@
+<?php
+    $library = 
+    [
+        [
+            'id' => 1,
+            'artiest' => 'Mavicwave',
+            'titel' => 'retrowave',
+            'leeftijd' => '30',
+            'image' => '/img/retrowave.jpg',
+            'genre' => 'rock'
+        ],
+        [
+            'id' => 2,
+            'artiest' => 'Daft punk',
+            'titel' => 'Random Acces Memories',
+            'leeftijd' => '48', '49',
+            'image' => '/img/DaftPunk.png',
+            'genre' => 'house/techno'
+        ]
+    ]
+?>  
---
+++ docker project folder/source/views/card.php
@@ -0,0 +1,9 @@
+
+    <section class="menuItem">
+        <figure class="cover">
+        <?= $single['titel'] ?><br>
+        <?= $single['artiest'] ?><br>
+            <img src="/img/retrowave.jpg" alt="Een albumcover van Retrowave">
+            <img src="/img/DaftPunk.png" alt="Een albumcover van Daft Punk">
+        </figure>
+    </section>
